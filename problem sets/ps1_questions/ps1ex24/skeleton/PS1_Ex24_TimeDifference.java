8d4565f66b8c9845ae878ad271fb75008612b1f0
benedictNg <benedictng1234@gmail.com>
2024-08-22 17:31:47 +0800

    Complete Q24

--- problem sets/ps1_questions/ps1ex24/skeleton/PS1_Ex24_TimeDifference.java
+++ problem sets/ps1_questions/ps1ex24/skeleton/PS1_Ex24_TimeDifference.java
@@ -2,25 +2,47 @@
  * CS1010J Programming Methodology
  * Problem Set 1 Exercise #24: PS1_Ex24_TimeDifference.java
  * 
- * <Fill in a short description of this program>
- * 
+ * <Finds time difference between two timings
  * 
  * <Type your name here>
  */
 
-import java.text.*;
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
 class TimeDifference {
-  
+
   public static void main(String[] args) {
-    
-    
-    
-    
+    int startHr, startMin, startSec, endHr, endMin, endSec, diffHr, diffMin, diffSec;
+    Scanner sc = new Scanner(System.in);
+    startHr = sc.nextInt();
+    startMin = sc.nextInt();
+    startSec = sc.nextInt();
+
+    endHr = sc.nextInt();
+    endMin = sc.nextInt();
+    endSec = sc.nextInt();
+
+    if (startSec > endSec) {
+      endMin--; // carry over a minute
+      diffSec = endSec + 60 - startSec;
+    } else {
+      diffSec = endSec - startSec;
+    }
+
+    if (startMin > endMin) {
+      endHr--; // carry over an hr
+      diffMin = endMin + 60 - startMin;
+    } else {
+      diffMin = endMin - startMin;
+    }
+
+    diffHr = endHr - startHr;
+
     // To show output as two digits with possible leading zero.
     // For example, df.format(8) will show 8 as 08
     DecimalFormat df = new DecimalFormat("00");
-    
-    System.out.println("Time difference: " +  ":" +  ":" );
+
+    System.out.println("Time difference: " + df.format(diffHr) + ":" + df.format(diffMin) + ":" + df.format(diffSec));
   }
 }
