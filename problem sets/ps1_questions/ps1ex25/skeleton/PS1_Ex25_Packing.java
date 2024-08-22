faa35d234a2513a3c0c7e042d16665359fbb4f65
benedictNg <benedictng1234@gmail.com>
2024-08-22 15:00:35 +0800

    Complete Qn 25

--- problem sets/ps1_questions/ps1ex25/skeleton/PS1_Ex25_Packing.java
+++ problem sets/ps1_questions/ps1ex25/skeleton/PS1_Ex25_Packing.java
@@ -8,21 +8,39 @@
  * <Type your name here>
  */
 
-
+import java.util.Scanner;
+import java.lang.Math;
 
 class Packing {
-  
+
   public static void main(String[] args) {
-    
-    
-    
+    int trayLength, trayWidth, slabLength, slabWidth;
+    int maxTileHori, maxTileVert;
+    Scanner sc = new Scanner(System.in);
+
     System.out.print("Enter size of tray: ");
-    
-    
+    trayLength = sc.nextInt();
+    trayWidth = sc.nextInt();
+
     System.out.print("Enter size of slab: ");
-    
-    
-    
-    System.out.println("Minimum unused area = ");
+    slabLength = sc.nextInt();
+    slabWidth = sc.nextInt();
+
+    maxTileHori = (trayLength / slabWidth) * (trayWidth / slabLength);
+
+    maxTileVert = (trayLength / slabLength) * (trayWidth / slabWidth);
+
+    if (area(trayLength, trayWidth) > area(slabLength, slabWidth)) {
+
+      int minArea = maxTileHori > maxTileVert ? area(trayLength, trayWidth) - maxTileHori * area(slabLength, slabWidth)
+          : area(trayLength, trayWidth) - maxTileVert * area(slabLength, slabWidth);
+
+      System.out.println("Minimum unused area = " + minArea);
+    }
+
+  }
+
+  public static int area(int length, int width) {
+    return length * width;
   }
 }
