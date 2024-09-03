565b30f066ae425d74681a7f90b64ee278a72116
benedictNg <benedictng1234@gmail.com>
2024-09-03 22:30:01 +0800

    complete qn 22

--- problem sets/ps2_questions/ps2ex22/skeleton/PS2_Ex22_Goldbach.java
+++ problem sets/ps2_questions/ps2ex22/skeleton/PS2_Ex22_Goldbach.java
@@ -8,36 +8,61 @@
  * <Type your name here>
  */
 
-import java.util.*;
+import java.util.Scanner;
 
 class Goldbach {
-  
+
   public static void main(String[] args) {
-    
-    
-    
+
+    Scanner sc = new Scanner(System.in);
+
     System.out.print("Enter n: ");
-    
-    
+    int n = sc.nextInt();
+
+    if (testConjecture(n)) {
+      System.out.println("true");
+    } else {
+      System.out.println("false");
+    }
+
   }
-  
+
   // Verify that every even number in range [4, n]
   // an be expressed as the sum of two primes.
   public static boolean testConjecture(int n) {
-    
-    return false;  // stub, to be replaced by your code
+    int firstDigit = 1, secondDigit;
+    while (firstDigit <= n / 2) {
+      secondDigit = n - firstDigit;
+      if (isPrime(firstDigit) && isPrime(secondDigit)) {
+        return true;
+      }
+      firstDigit++;
+    }
+    return false; // stub, to be replaced by your code
   }
-  
-  // Check whether num can be written as the sum of two primes.
-  // Return true if so, false otherwise.
-  public static boolean equalsSumOfTwoPrimes(int num) {
-    
-    return false;  // stub, to be replaced by your code
-  }
-  
+
   // Check if num is prime or not, return true if so, false otherwise.
   public static boolean isPrime(int num) {
-    
-    return false;  // stub, to be replaced by your code
+    // edge cases
+    if (num == 1) {
+      return false;
+    }
+    if (num == 2 || num == 3) {
+      return true;
+    }
+    if (num % 2 == 0 || num % 3 == 0) {
+      return false;
+    }
+    int i = 5;
+    // prime numbers are 6k +- 1
+    while (i * i <= num) {
+      // find all cases where num is divible by prime divisors
+      if (num % i == 0 || num % i + 2 == 0) {
+        return false;
+      }
+      i += 6;
+    }
+    return true;
+
   }
 }
