333f5e917214d72d1bdfb43a9616c090f4195c11
benedictNg <benedictng1234@gmail.com>
2024-09-03 16:30:21 +0800

    Add tutorial 3 + Problem sets 2 Q10, Q20

--- problem sets/ps2_questions/ps2ex10/skeleton/PS2_Ex10_Collatz.java
+++ problem sets/ps2_questions/ps2ex10/skeleton/PS2_Ex10_Collatz.java
@@ -8,25 +8,28 @@
  * <Type your name here>
  */
 
-import java.util.*;
+import java.util.Scanner;
 
 class Collatz {
-  
+
   public static void main(String[] args) {
-    
-    
-    
+    Scanner sc = new Scanner(System.in);
+
     System.out.print("Enter a natural number: ");
-    
-    
-    
-    
-    System.out.println("Number of iterations = " );
+    int n = sc.nextInt();
+    int numIterations = countIterations(n);
+
+    System.out.println("Number of iterations = " + numIterations);
   }
-  
+
   // Calculate how many rounds are needed to process n till 1
   public static int countIterations(int n) {
-    
-    return 0;  // stub, to be replaced by your code
+    int sum = 0;
+    while (n != 1) {
+      n = n % 2 == 0 ? n / 2 : 3 * n + 1;
+      sum++;
+    }
+
+    return sum; // stub, to be replaced by your code
   }
 }
--- problem sets/ps2_questions/ps2ex20/skeleton/PS2_Ex20_UCard.java
+++ problem sets/ps2_questions/ps2ex20/skeleton/PS2_Ex20_UCard.java
@@ -8,36 +8,61 @@
  * <Type your name here>
  */
 
-import java.util.*;
+import java.util.Scanner;
 
 class UCard {
-  
+
   public static void main(String[] args) {
-    
-    
-    
+
     System.out.print("Enter uCard Number: ");
-    
-    
-    
-    
-    System.out.println("The check number is " );
-    
-    
-    System.out.println("Valid");
-    
-    System.out.println("Invalid");
+    Scanner sc = new Scanner(System.in);
+    int input = sc.nextInt();
+    int checkDigit = luhnah(input);
+    System.out.println("The check number is " + checkDigit);
+
+    // no remainder when checkdigit divided by 7
+    if (checkDigit % 7 == 0) {
+      System.out.println("Valid");
+      printBranch(input);
+    } else {
+      System.out.println("Invalid");
+    }
   }
-  
-  // <Write a short description of the method here>
+
+  // <returns check digit based on luhnah algo>
   public static int luhnah(int uCardNo) {
-    
-    return 0;  // stub, to be replaced by your code
+    int pos = 0;
+    int checkSum = 0;
+    while (uCardNo > 0) {
+      int digit = uCardNo % 10;
+      // check if its every second digit and multiply the digit
+      if (pos % 2 != 0) {
+        digit *= 2;
+        while (digit > 0) {
+          checkSum += digit % 10;
+          digit /= 10;
+        }
+      } else {
+        checkSum += digit;
+      }
+      uCardNo /= 10;
+      pos++;
+    }
+    return checkSum; // stub, to be replaced by your code
   }
-  
+
   // <Write a short description of the method here>
   public static void printBranch(int uCard) {
-    
-    
+    // divide until left with first two digits
+    while (uCard >= 100) {
+      uCard /= 10;
+    }
+    if (uCard <= 35 && uCard >= 31) {
+      System.out.println("Issued by East branch");
+    } else if (uCard <= 55 && uCard >= 51) {
+      System.out.println("Issued by West branch");
+    } else {
+      System.out.println("Issued by Central branch");
+    }
   }
 }
