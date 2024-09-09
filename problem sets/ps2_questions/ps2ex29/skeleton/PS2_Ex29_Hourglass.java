f11b103ded4113c4fd480c78ccf41463943bea6b
benedictNg <benedictng1234@gmail.com>
2024-09-09 13:52:45 +0800

    complete qn 29

--- problem sets/ps2_questions/ps2ex29/skeleton/PS2_Ex29_Hourglass.java
+++ problem sets/ps2_questions/ps2ex29/skeleton/PS2_Ex29_Hourglass.java
@@ -11,23 +11,43 @@
 import java.util.*;
 
 class Hourglass {
-  
+
   public static void main(String[] args) {
-    
+
     Scanner sc = new Scanner(System.in);
-    
+
     System.out.print("Enter 3 inputs: ");
-    int hg1 = sc.nextInt();   // durations of hourglasses; assume hg1 < hg2
-    int hg2 = sc.nextInt();       
-    
-    int flips1;    // number of flips needed for hourglass 1
-    int flips2;    // number of flips needed for hourglass 2
-    
-    
-    
-    System.out.println("Impossible!");
-    
-    System.out.println(flips1 + " flip(s) for " + hg1 + "-minute hourglass and " +
-                       flips2 + " flip(s) for " + hg2 + "-minute hourglass.");
+    int hg1 = sc.nextInt(); // durations of hourglasses; assume hg1 < hg2
+    int hg2 = sc.nextInt();
+    int totalTime = sc.nextInt();
+
+    int flips1; // number of flips needed for hourglass 1
+    int flips2 = totalTime / hg2;
+    int remainTime = totalTime % hg2;
+
+    // case where remains time = shorter hour glass
+    if (remainTime % hg1 == 0) {
+      flips1 = remainTime / hg1;
+      System.out.println(flips1 + " flip(s) for " + hg1 + "-minute hourglass and " +
+          flips2 + " flip(s) for " + hg2 + "-minute hourglass.");
+      // case where remain time + one previous longer hourglass rolls back = n * short
+      // time hourglass
+    } else {
+      while ((remainTime < totalTime && (remainTime) % hg1 != 0)) {
+        remainTime += hg2;
+        flips2--;
+      }
+
+      // System.out.println(remainTime);
+      if (remainTime < totalTime) {
+        flips1 = remainTime / hg1;
+        System.out.println(flips1 + " flip(s) for " + hg1 + "-minute hourglass and " +
+            flips2 + " flip(s) for " + hg2 + "-minute hourglass.");
+
+      } else {
+        System.out.println("Impossible!");
+      }
+    }
+
   }
 }
