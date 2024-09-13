3f4173462cd5111a5af6423be9288d79b44c1f9a
benedictNg <benedictng1234@gmail.com>
2024-09-13 15:06:46 +0800

    add q21

--- problem sets/ps2_questions/ps2ex21/skeleton/PS2_Ex21_Legendre.java
+++ problem sets/ps2_questions/ps2ex21/skeleton/PS2_Ex21_Legendre.java
@@ -9,36 +9,68 @@
  */
 
 import java.util.*;
+import java.lang.*;
 
 class Legendre {
-  
+
   public static void main(String[] args) {
-    
-    
-    
+
     System.out.print("Enter n: ");
-    
-    
-    
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    if (testConjecture(n)) {
+      System.out.println("true");
+    } else {
+      System.out.println("false");
+    }
+    ;
+
   }
-  
+
   // Verify if there is at least one prime number in
   // each range (1^2 to 2^2), (2^2 to 3^2), (n^2 to (n+1)^2)
   public static boolean testConjecture(int n) {
-    
-    return false;  // stub, to be replaced by your code
+    for (int i = 1; i < n; i++) {
+      if (!hasPrime((int) Math.pow(n, 2), (int) Math.pow(n + 1, 2))) {
+        return false;
+      }
+    }
+    return true; // all contains a prime
   }
-  
+
   // Check whether there is any prime in range [low, high],
   // return true if so, or false otherwise
   public static boolean hasPrime(int low, int high) {
-    
-    return false;  // stub, to be replaced by your code
+    for (int i = low; i <= high; i++) {
+      if (isPrime(i)) {
+        return true;
+      }
+    }
+    return false; // range does not contain a prime number
   }
-  
-  // <Write a short description of the method here>
+
   public static boolean isPrime(int num) {
-    
-    return false;  // stub, to be replaced by your code
+    // edge cases
+    if (num == 1) {
+      return false;
+    }
+    if (num == 2 || num == 3) {
+      return true;
+    }
+    if (num % 2 == 0 || num % 3 == 0) {
+      return false;
+    }
+    int i = 5;
+    // prime numbers are 6k +- 1
+    while (i * i <= num) {
+      // find all cases where num is divible by prime divisors
+      if (num % i == 0 || num % i + 2 == 0) {
+        return false;
+      }
+      i += 6;
+    }
+    return true;
+
   }
+
 }
