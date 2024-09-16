0ffcab1ed6ef26f0c764459ddf89284f66e80503
benedictNg <benedictng1234@gmail.com>
2024-09-16 11:15:08 +0800

    complete ps3 q14

--- problem sets/ps3_questions/ps3ex14/skeleton/PS3_Ex14_SetContainment.java
+++ problem sets/ps3_questions/ps3ex14/skeleton/PS3_Ex14_SetContainment.java
@@ -11,36 +11,56 @@
 import java.util.*;
 
 class SetContainment {
-  
+
   public static void main(String[] args) {
-    
+
     // Note: CodeCrunch only accepts one Scanner object!
     // So create 'sc' here and pass it to scanArray method twice
     Scanner sc = new Scanner(System.in);
-    
+
     System.out.print("Size of 1st array? ");
-    int[] arrA = scanArray(sc);
-    
+    int sizeA = sc.nextInt();
+    int[] arrA = scanArray(sizeA, sc);
+
     System.out.print("Size of 2nd array? ");
-    int[] arrB = scanArray(sc);
-    
-    System.out.println("1st array is a subset of 2nd array");
-    
-    System.out.println("1st array is not a subset of 2nd array");
+    int sizeB = sc.nextInt();
+    int[] arrB = scanArray(sizeB, sc);
+
+    if (isSubset(arrA, arrB)) {
+      System.out.println("1st array is a subset of 2nd array");
+    } else {
+      System.out.println("1st array is not a subset of 2nd array");
+    }
+
   }
-  
+
   // Read elements into an array and return it
-  public static int[] scanArray(Scanner sc) {
-    
-    int size = sc.nextInt();
-    
+  public static int[] scanArray(int size, Scanner sc) {
     System.out.print("Enter " + size + " values: ");
-    
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++) {
+      array[i] = sc.nextInt();
+    }
+    return array;
   }
-  
-  // <Write a short description of the method here>
+
   public static boolean isSubset(int[] arrA, int[] arrB) {
-    
-    return false;  // stub
+    // loops through all the elements
+    for (int posA = 0; posA < arrA.length; posA++) {
+
+      boolean found = false;
+      // for loop iterates while arrA element does not exist within B
+      for (int posB = 0; posB < arrB.length && !found; posB++) {
+        if (arrA[posA] == arrB[posB]) {
+          found = true;
+        }
+      }
+      // breaks the function upon element not being in arrB
+      if (!found) {
+        return false;
+      }
+    }
+    // all elements exist in arrB
+    return true;
   }
 }
