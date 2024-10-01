bb2c7c5552adf7491e7dfcd1b30dc48d18c00259
benedictNg <benedictng1234@gmail.com>
2024-10-01 17:06:52 +0800

    complete q9, q17. q18

--- problem sets/ps3_questions/ps3ex09/skeleton/PS3_Ex09_Pair.java
+++ problem sets/ps3_questions/ps3ex09/skeleton/PS3_Ex09_Pair.java
@@ -11,23 +11,42 @@
 import java.util.*;
 
 class Pair {
-  
+
   public static void main(String[] args) {
-    
+    Scanner sc = new Scanner(System.in);
     System.out.print("Enter the number of distinct elements: ");
-    
+    int size = sc.nextInt();
+    int[] arr = new int[size];
     System.out.print("Enter " + " elements: ");
-    
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = sc.nextInt();
+    }
     System.out.print("Enter key: ");
-    
-    System.out.println("Exist");
-    
-    System.out.println("Not exist");
+    int key = sc.nextInt();
+
+    if (checkPair(arr, key)) {
+      System.out.println("Exist");
+    } else {
+      System.out.println("Not exist");
+    }
+
   }
-  
+
   // <Write a short description of the method here>
   public static boolean checkPair(int[] arr, int key) {
-    
-    return false;  // stub
+    int left = 0;
+    int right = arr.length - 1; // last element of list
+
+    while (left < right) {
+      int sum = arr[left] + arr[right];
+      if (sum == key) {
+        return true; // key is found
+      } else if (sum < key) {
+        left++;
+      } else { // sum > key
+        right--;
+      }
+    }
+    return false; // stub
   }
 }
--- problem sets/ps3_questions/ps3ex18/skeleton/PS3_Ex18_COE.java
+++ problem sets/ps3_questions/ps3ex18/skeleton/PS3_Ex18_COE.java
@@ -11,23 +11,47 @@
 import java.util.*;
 
 class COE {
-  
+
   public static void main(String[] args) {
-    
+    Scanner sc = new Scanner(System.in);
     System.out.print("Enter the number of available COEs: ");
-    
+    int numCOE = sc.nextInt();
     System.out.print("Enter the number of bids: ");
-    
+    int numBids = sc.nextInt();
+
+    int[] bids = new int[numBids];
     System.out.print("Enter " + " bids: ");
-    
+    for (int i = 0; i < bids.length; i++) {
+      bids[i] = sc.nextInt();
+    }
+    sortBids(bids);
+    int minBid = bids[numCOE - 1];
+
+    if (bids[numCOE] == minBid) {
+      minBid = bids[numCOE - 2]; // becomes next highest bid
+    }
+
     // for debugging
-    //System.out.println(Arrays.toString(bids));
-    
-    System.out.println("COE final price this month is $");
+    System.out.println(Arrays.toString(bids));
+
+    System.out.println("COE final price this month is $" + minBid);
   }
-  
+
   // Sort bids in descending order
   public static void sortBids(int[] bids) {
-    
+    int lastIndex = bids.length - 1;
+    while (lastIndex > 0) {
+      int lastExchangeIndex = 0;
+      for (int i = 0; i < lastIndex; i++) {
+        if (bids[i] < bids[i + 1]) {
+          int temp = bids[i];
+          bids[i] = bids[i + 1];
+          bids[i + 1] = temp;
+          lastExchangeIndex = i;
+        }
+      }
+      lastIndex = lastExchangeIndex;
+    }
+
   }
 }
