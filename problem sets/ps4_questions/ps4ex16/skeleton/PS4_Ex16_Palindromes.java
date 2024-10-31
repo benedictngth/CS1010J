394e1aae62b60c409a383ab4914e72b496b26d6f
benedictNg <benedictng1234@gmail.com>
2024-10-31 16:08:28 +0800

    add q16

--- problem sets/ps4_questions/ps4ex16/skeleton/PS4_Ex16_Palindromes.java
+++ problem sets/ps4_questions/ps4ex16/skeleton/PS4_Ex16_Palindromes.java
@@ -12,21 +12,41 @@
 import java.util.*;
 
 class Palindromes {
-  
+
   public static void main(String[] args) {
-    
+    Scanner sc = new Scanner(System.in);
     System.out.print("How many strings? ");
-    
-    System.out.println("Enter " +  " strings, each on a line:");        
-    
-    System.out.println("Number of palindromes = " );
+    int numStr = sc.nextInt();
+    sc.nextLine();
+    System.out.println("Enter " + numStr + " strings, each on a line:");
+    int palim = 0;
+    for (int i = 0; i < numStr; i++) {
+      String nextString = sc.nextLine();
+      if (isPalindrome(nextString.toLowerCase())) {
+        palim++;
+      }
+    }
+
+    System.out.println("Number of palindromes = " + palim);
   }
-  
-  
+
   // Check a single string 'str'.
   // Return true if str is a palindrome, or false otherwise.
   public static boolean isPalindrome(String str) {
-    
-    return false;  // stub
+    int startIdx = 0;
+    int endIdx = str.length() - 1;
+    while (startIdx < endIdx) {
+      if (!Character.isLetter(str.charAt(startIdx))) {
+        startIdx++;
+      } else if (!Character.isLetter(str.charAt(endIdx))) {
+        endIdx--;
+      } else if (str.charAt(startIdx) != str.charAt(endIdx)) {
+        return false;
+      } else {
+        startIdx++;π
+        endIdx--;
+      }
+    }
+    return true; // stub
   }
 }
