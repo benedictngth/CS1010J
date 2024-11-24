37b4dff727508fc5373c147d5223d39495c167a5
benedictNg <benedictng1234@gmail.com>
2024-11-24 17:41:35 +0800

    q27,28

--- problem sets/ps4_questions/ps4ex27/skeleton/PS4_Ex27_OddDigitSum.java
+++ problem sets/ps4_questions/ps4ex27/skeleton/PS4_Ex27_OddDigitSum.java
@@ -11,20 +11,33 @@
 import java.util.*;
 
 class OddDigitSum {
-  
+
   public static void main(String[] args) {
-    
+    Scanner sc = new Scanner(System.in);
     System.out.print("Enter a positive integer: ");
-    
-    System.out.println("Sum of digits for " + " is odd");
-    
-    System.out.println("Sum of digits for " + " is even");
+    int num = sc.nextInt();
+
+    if (isDigitSumOdd(num)) {
+      System.out.println("Sum of digits for " + " is odd");
+    } else {
+      System.out.println("Sum of digits for " + num + " is even");
+    }
+
   }
-  
+
   // Return ture if the sum of all digits is odd, or false otherwise.
   // Pre-cond: num > 0
   public static boolean isDigitSumOdd(int num) {
-    
-    return false;  // stub
+    // one digit
+    if (num < 10) {
+      return num % 2 == 1;
+    } else {
+      // even + odd = odd (T)
+      // odd + odd = even (F)
+      // even + even = even (F)
+
+      return isDigitSumOdd(num % 10) == !isDigitSumOdd(num / 10);
+    }
+
   }
 }
